95887d74b468606d3c922d51e1744991696f4824
Aethar <elliott.ashby88@gmail.com>
2025-10-23 11:07:45 +0100

    pure functions

--- main.py
+++ main.py
@@ -4,56 +4,44 @@ from qiskit import QuantumCircuit, transpile
 import numpy as np
 from qiskit_aer import AerSimulator
 from matplotlib import pyplot as plt
+from scipy.special import ellipk
+from tqdm import tqdm
+from sys import argv
 
 
-# CONSTANTS
-pi = np.pi
-N = 16
-g = 1.0
-m = 0.5
-theta = 0.0
-a = 1.0
-w = 1/(2*a)
-J = g**2 * a / 2
-
-# ADIABATIC PARAMETERS
-T = 5
-dt = 0.1
-steps = int(T/dt)
-m0 = 1.0
-
-
-# GATES
 def zz_gate(circ, i, j, coeff):
-    """
-    Implements exp(-i J dt Z_i Z_j / 2)
-    """
+    """Implements exp(-i J dt Z_i Z_j / 2)."""
+    circ = circ.copy()
     circ.cx(i, j)
     circ.rz(2 * coeff, j)
     circ.cx(i, j)
+    return circ
 
 
 def xy_gate(circ, i, j, coeff):
-    """
-    Implements exp(-i w dt (X_i X_j + Y_i Y_j) / 2)
-    """
+    """Implements exp(-i w dt (X_i X_j + Y_i Y_j) / 2)."""
+    circ = circ.copy()
+    pi = np.pi
     circ.cx(i, j)
     circ.rx(2 * coeff, i)
     circ.cx(i, j)
 
-    circ.rz(-pi/2, i)
-    circ.rz(-pi/2, j)
+    circ.rz(-pi / 2, i)
+    circ.rz(-pi / 2, j)
     circ.cx(i, j)
     circ.rx(2 * coeff, i)
     circ.cx(i, j)
-    circ.rz(pi/2, i)
-    circ.rz(pi/2, j)
+    circ.rz(pi / 2, i)
+    circ.rz(pi / 2, j)
+    return circ
 
 
-def trotter_step(circ, t):
+def trotter_step(circ, t, theta, *, N, T, dt, w, m0, m, J):
     """
+    Performs one Trotter step:
     U(t) = exp(-i H_+-(t) dt / 2) exp(-i H_ZZ dt) exp(-i H_Z dt) exp(-i H_+-(t) dt / 2)
     """
+    circ = circ.copy()
     t_frac = t / T
     wt = w * t_frac
     mt = (1 - t_frac) * m0 + t_frac * m
@@ -61,45 +49,26 @@ def trotter_step(circ, t):
 
     # H_+- half-time evolution
     for n in range(N - 1):
-        coeff = 0.5 * dt * (wt - ((-1)**n) * (mt/2) * np.sin(thetat))
-        xy_gate(circ, n, n + 1, coeff)
+        coeff = 0.5 * dt * (wt - ((-1) ** n) * (mt / 2) * np.sin(thetat))
+        circ = xy_gate(circ, n, n + 1, coeff)
 
     # H_ZZ full-time evolution
     for n in range(1, N):
         for k in range(n):
-            zz_gate(circ, k, n, J * dt / 2)
+            circ = zz_gate(circ, k, n, J * dt / 2)
 
     # H_Z full-time evolution
     for n in range(N):
-        coeff = 0.5 * dt * (mt * np.cos(thetat) * (-1)**n)
+        coeff = 0.5 * dt * (mt * np.cos(thetat) * (-1) ** n)
         circ.rz(2 * coeff, n)
 
     # H_+- half-time evolution
     for n in range(N - 1):
-        coeff = 0.5 * dt * (wt - ((-1)**n) * (mt/2) * np.sin(thetat))
-        xy_gate(circ, n, n + 1, coeff)
+        coeff = 0.5 * dt * (wt - ((-1) ** n) * (mt / 2) * np.sin(thetat))
+        circ = xy_gate(circ, n, n + 1, coeff)
 
+    return circ
 
-# qc = QuantumCircuit(N)
-#
-# # initial satate |0101...>
-# for n in range(1, N, 2):
-#     qc.x(n)
-#
-# for step in range(steps):
-#     trotter_step(qc, step * dt)
-#
-# qc.barrier()
-# qc.measure_all()
-#
-# # print(qc.draw())
-#
-# sim = AerSimulator()
-# compiled = transpile(qc, sim)
-# result = sim.run(compiled, shots=1000).result()
-# counts = result.get_counts()
-#
-# plot_histogram(counts, filename="_histogram.png")
 
 def compute_z_expectations(counts, N, shots):
     """Return <Z_n> for each qubit n from measurement counts."""
@@ -112,41 +81,56 @@ def compute_z_expectations(counts, N, shots):
     return expectations
 
 
-def chiral_condensate(expectations, a=1.0):
+def chiral_condensate(expectations, a):
     N = len(expectations)
     vals = [(-1) ** n * expectations[n] for n in range(N)]
     return np.sum(vals) / (2 * N * a)
 
 
-def run_sim(g, m, theta, shots=2048):
+def run_sim(g, m, theta, *, N, T, dt, m0, a, w, shots=2048):
+    """Run one simulation for given parameters and return the VEV - VEV_free."""
+    J = g**2 * a / 2
+
     qc = QuantumCircuit(N)
-    # initial state |0101...>
+    # Initial state |0101...>
     for n in range(1, N, 2):
         qc.x(n)
 
+    steps = int(T / dt)
     for step in range(steps):
-        trotter_step(qc, step * dt)
+        qc = trotter_step(qc, step * dt, theta, N=N, T=T,
+                          dt=dt, w=w, m0=m0, m=m, J=J)
 
     qc.measure_all()
+
     sim = AerSimulator()
     compiled = transpile(qc, sim)
     result = sim.run(compiled, shots=shots).result()
     counts = result.get_counts()
 
-    # compute <Z_n> and chiral condensate
     expectations = compute_z_expectations(counts, N, shots)
-    psi_bar_psi = chiral_condensate(expectations)
-    return psi_bar_psi
+    psi_bar_psi = chiral_condensate(expectations, a)
+    psi_free = -(m * np.cos(theta) / np.pi) * \
+        1 / np.sqrt(1 + (m*a*np.cos(theta))**2) * \
+        ellipk((1 - (m*a*np.sin(theta))**2) / (1 + (m*a*np.cos(theta))**2))
+    psi_bar_psi_minus_free = psi_bar_psi - psi_free
+    return psi_bar_psi_minus_free
 
 
-def run_sims(g, m):
+def run_sims(g, m, *, N, T, dt, m0, a, w):
+    """Run multiple simulations across a theta range."""
     thetas = [i * 0.05 * 2 * np.pi for i in range(0, 11)]
-    results = [run_sim(g, m, theta) for theta in thetas]
+    results = []
+    for theta in tqdm(thetas):
+        res = run_sim(g, m, theta, N=N, T=T, dt=dt, m0=m0, a=a, w=w)
+        results.append(res)
     return np.array(thetas), np.array(results)
 
 
-def plot_results(thetas, results):
+def plot_results(thetas, results, **kwargs):
     plt.plot(thetas / (2 * np.pi), results, "o-", label="simulation")
+    plt.title(f"Paper figure 4 with T={kwargs['T']}, dt={
+              kwargs['dt']}, g={kwargs['g']}, m={kwargs['m']}")
     plt.xlabel(r"$\theta / 2\pi$")
     plt.ylabel(r"$\langle\bar{\psi}\psi\rangle$")
     plt.legend()
@@ -155,6 +139,22 @@ def plot_results(thetas, results):
     plt.show()
 
 
+def main():
+    a = 1.0
+    params = {
+        "N": 16,
+        "a": a,
+        "w": 1.0 / (2 * a),
+        "T": 150,
+        "dt": 0.3,
+        "m0": 1.0,
+        "g": 1.0,
+        "m": 0.1
+    }
+
+    thetas, results = run_sims(**params)
+    plot_results(thetas, results, **params)
+
+
 if __name__ == "__main__":
-    thetas, results = run_sims(g=1.0, m=0.1)
-    plot_results(thetas, results)
+    main()
