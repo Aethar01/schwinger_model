7bf278ea44c59e372651a935dcfb5f64c21ab40c
Aethar <elliott.ashby88@gmail.com>
2025-10-25 16:15:13 +0100

    new initial state

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from matplotlib import pyplot as plt
 from scipy.special import ellipk
 from tqdm import tqdm
 import argparse
-
-import math
+from qiskit.quantum_info import Statevector
 
 
 def zz_gate(circ, i, j, coeff):
@@ -65,6 +64,12 @@ def xxyy_gate(circ, i, j, coeff):
     return circ
 
 
+def c_n(t, T, m0, m, theta, n, J, N):
+    cn = 1/2 * ((1 - t / T) * m0 + t / T * m) * np.cos(t / T * theta) * \
+        ((-1) ** n) - J / 2 * sum([k % 2 for k in range(n, N-1)])
+    return cn
+
+
 def trotter_step(circ, t, theta, *, N, T, dt, w, m0, m, J):
     """
     Performs one Trotter step:
@@ -77,15 +82,10 @@ def trotter_step(circ, t, theta, *, N, T, dt, w, m0, m, J):
                                                 * m0 + m) * np.sin(theta * t / T)
         return wbar
 
-    def c_n(t, T, m0, m, theta, n, J):
-        cn = 1/2 * ((1 - t / T) * m0 + t / T * m) * np.cos(t / T * theta) * \
-            ((-1) ** n) - J / 2 * sum([k % 2 for k in range(n, N-1)])
-        return cn
-
     def H_Z(circ):
         circ.copy()
         for n in range(N):
-            cn = c_n(t, T, m0, m, theta, n, J)
+            cn = c_n(t, T, m0, m, theta, n, J, N)
             circ.rz(2 * cn * dt, n)
         return circ
 
@@ -166,23 +166,48 @@ def chiral_condensate(expectations, a):
     return np.sum(vals) / (2 * N * a)
 
 
-def run_sim(g, m, theta, *, N, T, dt, m0, a, w, shots, draw: bool):
+def run_sim(g, m, theta, *, N, T, dt, m0, a, w, shots, draw: bool, t=None):
     """Run one simulation for given parameters and return the VEV - VEV_free."""
     J = g**2 * a / 2
 
     qc = QuantumCircuit(N)
-    # Initial state |0101...>
-    for n in range(1, N, 2):
-        qc.x(n)
 
-    steps = int(T / dt)
+    dim = 2 ** N
+
+    state_vector = np.zeros(dim, dtype=complex)
+
+    for n in range(N):
+        state_vector[n] = c_n(t, T, m0, m, theta, n, J, N)
+
+    norm = np.linalg.norm(state_vector)
+
+    if norm < 1e-9:
+        print("Warning: State vector norm is close to zero.")
+        exit(1)
+    else:
+        normalized_state = state_vector / norm
+
+    state = Statevector(normalized_state)
+    qc.initialize(state)
+
+    # Initial state |0101...>
+    # for n in range(1, N, 2):
+    #     qc.x(n)
+
+    for n in range(N):
+        cn = c_n(t, T, m0, m, theta, n, J, N)
+        qc.rz(2 * cn * dt, n)
+
+    steps = int(t / dt)
+
     for step in range(steps):
         qc = trotter_step(qc, step * dt, theta, N=N, T=T,
                           dt=dt, w=w, m0=m0, m=m, J=J)
 
     qc.measure_all()
 
-    if draw:
+    # print(steps)
+    if steps == 1 and draw:
         print(qc.draw(output="mpl"))
         plt.show()
         exit()
@@ -191,6 +216,8 @@ def run_sim(g, m, theta, *, N, T, dt, m0, a, w, shots, draw: bool):
     compiled = transpile(qc, sim)
     result = sim.run(compiled, shots=shots).result()
     counts = result.get_counts()
+
+    # print(f"t={t}, counts={counts}")
 
     expectations = compute_z_expectations(counts, N, shots)
     psi_bar_psi = chiral_condensate(expectations, a)
@@ -209,20 +236,22 @@ def run_sims_theta(g, m, *, N, T, dt, m0, a, w, shots, draw: bool, **kwargs):
     """Run multiple simulations across a theta range."""
     thetas = [i * 0.05 * 2 * np.pi for i in range(0, 11)]
     results = []
+    t = T / dt
     for theta in tqdm(thetas):
         res = run_sim(g, m, theta, N=N, T=T, dt=dt,
-                      m0=m0, a=a, w=w, shots=shots, draw=draw)
+                      m0=m0, a=a, w=w, shots=shots, draw=draw, t=t)
         results.append(res)
     return np.array(thetas), np.array(results)
 
 
 def run_sims_t(g, m, *, N, T, dt, m0, a, w, shots, draw: bool, **kwargs):
     """Run multiple simulations across a t range."""
-    ts = [i * dt for i in range(int(T / dt))]
+    ts = [i * dt for i in range(int(5 / dt))]
+    # print(ts)
     results = []
     for t in tqdm(ts):
-        res = run_sim(g, m, t, N=N, T=T, dt=dt,
-                      m0=m0, a=a, w=w, shots=shots, draw=draw)
+        res = run_sim(g, m, kwargs["theta"], N=N, T=T, dt=dt,
+                      m0=m0, a=a, w=w, shots=shots, draw=draw, t=t)
         results.append(res)
     return np.array(ts), np.array(results)
 
@@ -270,11 +299,11 @@ def plot_results_t(ts, results_by_t, results_inf, **kwargs):
                  "o-", label="N->inf extrapolated")
     else:
         for res in results_by_t:
-            ts: np.ndarray
-            idx = math.ceil(5 / (kwargs["dt"]))
-            ts = ts[:idx]
-            res = res[:idx]
-            plt.plot(ts, res, "o-", label=f"T={kwargs['T']}, dt={kwargs['dt']}")
+            # idx = math.ceil(5 / (kwargs["dt"]))
+            # ts = ts[:idx]
+            # res = res[:idx]
+            plt.plot(ts, res, "o-",
+                     label=f"T={kwargs['T']}, dt={kwargs['dt']}")
 
     plt.title(f"(g,m,N,w) = ({kwargs['g']},{
               kwargs['m']},{kwargs['N']},{kwargs['w']})")
@@ -319,6 +348,8 @@ def parse_args():
                         help="Save figure to this path")
     parser.add_argument("-v", "--verbose", action="store_true",
                         help="Print parameters")
+    parser.add_argument("-t", "--theta", type=float,
+                        default=0.0, help="Initial angle")
     return parser.parse_args()
 
 
@@ -341,7 +372,8 @@ def main():
         "m": args.mass or 0.0,
         "shots": args.shots or 2048,
         "draw": args.draw or False,
-        "output": args.output or None
+        "output": args.output or None,
+        "theta": args.theta or 0.0
     }
 
     if args.verbose:
